70c03ada63362037eaf08cdc8b90e8092aed93c7
KayT256 <96287500+KayT256@users.noreply.github.com>
2024-02-07 12:26:10 -0600

    Upload index.html, script.js and styles.css

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed("#introduction-line", {
+        strings: ["Hello", "I'm Trieu Khang Trat"],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true,
+    });
+});
